f3f4491ec4231a6b2cb6eaea7d0f22fd54cd88c0
PierreCatrain <pierre.catrain@protonmail.com>
2024-05-08 19:39:40 +0200

    on redirige vers wordpress mais il y a pas de bdd

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,17 +1,71 @@
 #!/bin/sh
 
-mysql -e service mysql start;
+#mysql_install_db
+mysql_upgrade
+#/etc/init.d/mysql start
+#service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+#Check if the database exists
 
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then 
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	echo "Database already exists"
+else
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Set root option so that connexion without root password is not possible
 
-mysql -e "FLUSH PRIVILEGES;"
+mysql_secure_installation << _EOF_
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+Y
+root4life
+root4life
+Y
+n
+Y
+Y
+_EOF_
 
-exec mysqld_safe
+#Add a root user on 127.0.0.1 to allow remote connexion 
+#Flush privileges allow to your sql tables to be updated automatically when you modify it
+#mysql -uroot launch mysql command line client
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+#Create database and user in the database for wordpress
+
+echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE; GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+
+#Import database in the mysql command line
+mysql -uroot -p$SQL_ROOT_PASSWORD $SQL_DATABASE < /usr/local/bin/wordpress.sql
+
+fi
+
+service mysql stop
+#/etc/init.d/mysql stop
+
+exec "$@"
+
+
+
+
+
+##!/bin/sh
+#
+#mysql_upgrade
+#
+##/etc/init.d/mysql start
+#service mysql start
+#
+#mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+#
+#mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+#
+#mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+#
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#
+#mysql -e "FLUSH PRIVILEGES;"
+#
+#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+#
+#exec mysqld --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+#check if wp-config.php exist
 if [ -f ./wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
-    cd /var/www/wordpress
+
+	wget https://fr.wordpress.org/wordpress-6.0-fr_FR.tar.gz
+	tar -xvf wordpress-6.0-fr_FR.tar.gz
+	mv wordpress/* .
+	rm -rf wordpress
+
 	sed -i "s/votre_utilisateur_de_bdd/$SQL_USER/g" wp-config-sample.php
 	sed -i "s/votre_mdp_de_bdd/$SQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/votre_nom_de_bdd/$SQL_DATABASE/g" wp-config-sample.php
 	mv wp-config-sample.php wp-config.php
-
 fi
 
-# exec "$@"
+exec "$@"
