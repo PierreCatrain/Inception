9471e36080db5a1dd1e008e012da6d6c9a28acb8
PierreCatrain <pierre.catrain@protonmail.com>
2024-05-11 23:15:24 +0200

    nginx wordpress et mariadb ok, petit soucis avec le volume de mariadb

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,21 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-#check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
+	sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
+	chown -R www-data:www-data /var/www/*;
+	chown -R 755 /var/www/*;
+	mkdir -p /run/php/;
+	touch /run/php/php7.4-fpm.pid;
 
-	wget https://fr.wordpress.org/wordpress-6.0-fr_FR.tar.gz
-	tar -xvf wordpress-6.0-fr_FR.tar.gz
-	mv wordpress/* .
-	rm -rf wordpress
-
-	sed -i "s/votre_utilisateur_de_bdd/$SQL_USER/g" wp-config-sample.php
-	sed -i "s/votre_mdp_de_bdd/$SQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/votre_nom_de_bdd/$SQL_DATABASE/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+if [ ! -f /var/www/html/wp-config.php ]; then
+	echo "Wordpress: setting up..."
+	mkdir -p /var/www/html
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+	chmod +x wp-cli.phar; 
+	mv wp-cli.phar /usr/local/bin/wp;
+	cd /var/www/html;
+	wp core download --allow-root;
+	mv /var/www/wp-config.php /var/www/html/
+	echo "Wordpress: creating users.."
+	wp core install --allow-root --url=${DOMAIN_NAME} --title=${DATABASE} --admin_user=${ADMIN} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_MAIL}
+	wp user create --allow-root ${USER} ${USER_MAIL} --user_pass=${USER_PASSWORD};
+	echo "Wordpress: set up!"
 fi
 
 exec "$@"
