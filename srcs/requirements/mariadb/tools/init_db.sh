05c189acae8ef32adacf56761019604a8eae3501
picatrai <picatrai@myVM.myguest.virtualbox.org>
2025-03-27 16:06:50 +0100

    bientot la fin

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -3,13 +3,17 @@
 mysqld_safe --skip-networking &
 sleep 5
 
-mysql -u root <<EOF
+echo "1"
+
+mysql -u root -p"$DB_ROOT_PASSWORD"<<EOF
 CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_USER}\`@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
+
+echo "2"
 
 mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
 
--- srcs/requirements/wordpress/tools/sed_conf.sh
+++ srcs/requirements/wordpress/tools/sed_conf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sleep 10
+
+# on reconfigure a chaque fois ??
 sed -i "s/'votre_nom_de_bdd'/'${DB_DATABASE}'/" /var/www/wordpress/wp-config.php
 sed -i "s/'votre_utilisateur_de_bdd'/'${DB_USER}'/" /var/www/wordpress/wp-config.php
 sed -i "s/'votre_mdp_de_bdd'/'${DB_USER_PASSWORD}'/" /var/www/wordpress/wp-config.php
@@ -7,5 +10,30 @@ sed -i "s/'localhost'/'${DB_HOST}'/" /var/www/wordpress/wp-config.php
 sed -i "s/'wp_'/'${DB_TABLE_PREFIX}'/" /var/www/wordpress/wp-config.php
 
 # cat /var/www/wordpress/wp-config.php
+
+
+# wp user create \
+#         --allow-root \
+#         --dbname="$DB_DATABASE" \
+#         --dbuser="$WP_USER" \
+#         --dbpass="$WP_USER_PASSWORD" \
+#         --dbhost=mariadb:3306 --path="$WP_PATH"
+
+# echo "1"
+
+cd /var/www/wordpress
+
+wp core install \
+        --path="$WP_PATH" \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --allow-root
+
+echo "2"
+
+wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD" --porcelain
 
 /usr/sbin/php-fpm7.4 -F
