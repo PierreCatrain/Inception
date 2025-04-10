d7513b7deb3a5dfcca62b5121df27d19606e7a9c
picatrai <picatrai@myVM.myguest.virtualbox.org>
2025-04-10 06:45:11 +0200

    c'est pour ajd

--- srcs/requirements/wordpress/tools/sed_conf.sh
+++ srcs/requirements/wordpress/tools/sed_conf.sh
@@ -13,7 +13,6 @@ sed -i "s/'votre_mdp_de_bdd'/'${DB_USER_PASSWORD}'/" /var/www/wordpress/wp-confi
 sed -i "s/'localhost'/'${DB_HOST}'/" /var/www/wordpress/wp-config.php
 sed -i "s/'wp_'/'${DB_TABLE_PREFIX}'/" /var/www/wordpress/wp-config.php
 
-
 cd /var/www/wordpress
 
 wp core install \
